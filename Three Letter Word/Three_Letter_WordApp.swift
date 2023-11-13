8d8ccd33e3201ae0230f5eb4e78d2cbfac8f3f3c
Tom Bredemeier <tbredemeier@barrington220.org>
2023-11-13 09:42:35 -0600

    Initial Commit

---
+++ Three Letter Word/Three_Letter_WordApp.swift
@@ -0,0 +1,17 @@
+//
+//  Three_Letter_WordApp.swift
+//  Three Letter Word
+//
+//  Created by Tom Bredemeier on 11/13/23.
+//
+
+import SwiftUI
+
+@main
+struct Three_Letter_WordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
