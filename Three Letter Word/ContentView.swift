13eb921a4f19a5babc624e31d541f693071fb034
Tom Bredemeier <tbredemeier@barrington220.org>
2023-11-13 10:04:37 -0600

    Added red letter boxes

--- Three Letter Word/ContentView.swift
+++ Three Letter Word/ContentView.swift
@@ -10,12 +10,31 @@ import SwiftUI
 struct ContentView: View {
     @State private var letter = ""
     @State private var counter = 0
+    @State private var selectedLetters = ["", "", ""]
     let alphabet =  "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     var body: some View {
         VStack {
             Text("Three Letter Word")
                 .font(.title).bold()
             Text("Tap the gray box to change the letter")
+            CustomLetterBox(color: .gray, text: letter)
+                .onTapGesture {
+                    let position = alphabet.index(alphabet.startIndex, offsetBy: counter)
+                    letter = String(alphabet[position])
+                    counter += 1
+                    if counter == alphabet.count {
+                        counter = 0
+                    }
+                }
+            Text("Tap a red box to select that letter")
+            HStack {
+                ForEach(0..<3) {index in
+                    CustomLetterBox(color: .red, text: selectedLetters[index])
+                        .onTapGesture {
+                            selectedLetters[index] = letter
+                        }
+                }
+            }
         }
     }
 }
